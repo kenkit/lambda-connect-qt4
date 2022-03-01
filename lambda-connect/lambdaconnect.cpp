e57b23f11f897b74e45be7169603044b689f56c0
🆂🅰🅶🅴#6154 <kkituyi@yahoo.com>
2022-03-02 01:59:20 +0300

    Update

--- lambda-connect/lambdaconnect.cpp
+++ lambda-connect/lambdaconnect.cpp
@@ -1,19 +1,37 @@
 #include "lambdaconnect.h"
 
+namespace Qt4LambdaConnecter {
+
 LambdaConnectorHelper::LambdaConnectorHelper(QObject *parent,
-                                             const char* signal,
-                                             const std::function<void()>& f,
-                                             Qt::ConnectionType type)
+        const char* signal,
+        const std::function<void(int)>& f,
+        Qt::ConnectionType type)
     : QObject(parent),
-    m_signal(signal),
-    m_lambda(f)
+      m_signal(signal),
+      m_lambda(f)
+{
+    m_result = QObject::connect(parent, signal, this, SLOT(trigger(int)), type);
+}
+
+LambdaConnectorHelper::LambdaConnectorHelper(QObject *parent,
+        const char* signal,
+        const std::function<void()>& f,
+        Qt::ConnectionType type)
+    : QObject(parent),
+      m_signal(signal),
+      m_lambda1(f)
 {
     m_result = QObject::connect(parent, signal, this, SLOT(trigger()), type);
 }
 
+void LambdaConnectorHelper::trigger(int i)
+{
+    m_lambda(i);
+}
+
 void LambdaConnectorHelper::trigger()
 {
-    m_lambda();
+    m_lambda1();
 }
 
 bool LambdaConnectorHelper::connected()
@@ -25,18 +43,35 @@ bool LambdaConnectorHelper::disconnect()
 {
     if (m_result)
     {
+#if QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+        m_result = QObject::disconnect(parent(),
+                                       m_signal.toUtf8().data(),
+                                       this,
+                                       SLOT(trigger()));
+#else
         m_result = QObject::disconnect(parent(),
                                        m_signal.toAscii().data(),
                                        this,
                                        SLOT(trigger()));
+#endif
     }
     return m_result;
 }
 
 LambdaConnectorHelper* connect(QObject *sender,
-                        const char *signal,
-                        const std::function<void()> &lambda,
-                        Qt::ConnectionType type)
+                               const char *signal,
+                               const std::function<void(int)> &lambda,
+                               Qt::ConnectionType type)
 {
     return new LambdaConnectorHelper(sender, signal, lambda, type);
+}
+
+LambdaConnectorHelper* connect(QObject *sender,
+                               const char *signal,
+                               const std::function<void()> &lambda,
+                               Qt::ConnectionType type)
+{
+    return new LambdaConnectorHelper(sender, signal, lambda, type);
+}
+
 }
--- lambda-connect/lambdaconnect.h
+++ lambda-connect/lambdaconnect.h
@@ -4,10 +4,15 @@
 #include <QString>
 #include <QObject>
 
+namespace Qt4LambdaConnecter {
+
 class LambdaConnectorHelper: public QObject
 {
     Q_OBJECT
 public:
+    LambdaConnectorHelper(QObject *parent, const char* signal,
+                          const std::function<void(int)>& f,
+                          Qt::ConnectionType type = Qt::AutoConnection);
     LambdaConnectorHelper(QObject *parent, const char* signal,
                           const std::function<void()>& f,
                           Qt::ConnectionType type = Qt::AutoConnection);
@@ -15,10 +20,13 @@ public:
     bool connected();
 public slots:
     void trigger();
+    void trigger(int i);
 private:
     bool m_result;
     const QString m_signal;
-    std::function<void()> m_lambda;
+
+    std::function<void(int)> m_lambda;
+    std::function<void()> m_lambda1;
 
 };
 
@@ -26,6 +34,13 @@ LambdaConnectorHelper* connect(QObject *sender,
                                const char *signal,
                                const std::function<void()> &lambda,
                                Qt::ConnectionType type = Qt::AutoConnection);
+
+LambdaConnectorHelper* connect(QObject *sender,
+                               const char *signal,
+                               const std::function<void(int)> &lambda,
+                               Qt::ConnectionType type = Qt::AutoConnection);
+
+} // Qt4LambdaConnecter
 
 #endif // _LAMBDA_CONNECT_H_
 
